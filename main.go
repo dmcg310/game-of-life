1163b79ad45d948bc94051ced7af5fa13016bf5e
dmcg310 <120114728+dmcg310@users.noreply.github.com>
2024-01-11 12:22:05 +0000

    feat: start game loop & render grid

--- main.go
+++ main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"github.com/gdamore/tcell/v2"
 	"log"
+	"time"
 )
 
-type game struct {
-	isRunning bool
-	terminal  terminal
+type grid struct {
+	cells          [][]bool
+	needsRefreshed bool
 }
 
 type terminal struct {
@@ -15,7 +16,18 @@ type terminal struct {
 	height int
 }
 
+type game struct {
+	isRunning bool
+	grid      grid
+	screen    tcell.Screen
+	terminal  terminal
+}
+
 func main() {
+	initScreen().gameLoop()
+}
+
+func initScreen() *game {
 	screen, err := tcell.NewScreen()
 	if err != nil {
 		reportError(err)
@@ -25,45 +37,83 @@ func main() {
 		reportError(err)
 	}
 
-	defStyle := tcell.StyleDefault.Background(tcell.ColorReset).Foreground(tcell.ColorReset)
+	defStyle := tcell.StyleDefault.Background(tcell.ColorReset).
+		Foreground(tcell.ColorReset)
 
 	screen.SetStyle(defStyle)
 	screen.EnableMouse()
-	screen.EnablePaste()
 	screen.Clear()
 
-	quit := func() {
+	w, h := screen.Size()
+
+	cells := make([][]bool, w)
+	for i := range cells {
+		cells[i] = make([]bool, h)
+	}
+
+	return &game{
+		isRunning: true,
+		grid: grid{
+			cells:          cells,
+			needsRefreshed: true,
+		},
+		screen: screen,
+		terminal: terminal{
+			width:  w,
+			height: h,
+		},
+	}
+}
+
+func (g *game) gameLoop() {
+	quit := func(screen tcell.Screen) {
 		maybePanic := recover()
 		screen.Fini()
 		if maybePanic != nil {
 			panic(maybePanic)
 		}
 	}
-	defer quit()
+	defer quit(g.screen)
 
-	w, h := screen.Size()
-
-	g := &game{
-		isRunning: true,
-		terminal: terminal{
-			width:  w,
-			height: h,
-		},
-	}
+	delay := time.Millisecond * 100 // 100ms
 
 	for g.isRunning {
-		screen.Show()
-		ev := screen.PollEvent()
+		g.screen.Show()
+		ev := g.screen.PollEvent()
+
+		if g.grid.needsRefreshed {
+			g.renderGamestate()
+			g.screen.Show()
+		}
 
 		switch ev := ev.(type) {
 		case *tcell.EventResize:
-			screen.Sync()
+			g.screen.Sync()
 		case *tcell.EventKey:
-			if ev.Key() == tcell.KeyEscape || ev.Key() == tcell.KeyCtrlC || ev.Rune() == 'q' {
-				return
+			if ev.Key() == tcell.KeyEscape ||
+				ev.Key() == tcell.KeyCtrlC ||
+				ev.Rune() == 'q' {
+				g.isRunning = false
 			}
 		}
+
+		time.Sleep(delay)
 	}
+}
+
+func (g *game) renderGamestate() {
+	for x := 0; x < len(g.grid.cells); x++ {
+		for y := 0; y < len(g.grid.cells[x]); y++ {
+			char := ' '
+			if g.grid.cells[x][y] {
+				char = 'x'
+			}
+
+			g.screen.SetContent(x, y, char, nil, tcell.StyleDefault)
+		}
+	}
+
+	g.grid.needsRefreshed = false
 }
 
 func reportError(msg error) {
